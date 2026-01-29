1446d0d748821ccd384f94f5dd4a19cebcfdbb1a
reoanrudo <sodapon03@gmail.com>
2026-01-29 19:55:33 +0900

    fix: Remove await from initialize call to fix build error

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
           setWebrtc(service);
           
           service.onConnected(() => setConnected(true));
-          await service.initialize(false);
+          service.initialize(false);
         }
       }
     };
